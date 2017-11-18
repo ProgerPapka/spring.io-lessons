3da095d5c7cafb6d6c343a0ffabdb7f1040ee58b
Эльдар <ziatdinov.eldar9@gmail.com>
2017-11-18 22:35:30 +0400

    Added example to use ioc container

---
+++ src/main/java/lessons/two/Starter.java
@@ -0,0 +1,20 @@
+package lessons.two;
+
+import lessons.two.services.GreetingService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Starter {
+
+    private static final Logger logger = LogManager.getLogger(Starter.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting configuration...");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(LessonsConfiguration.class);
+        GreetingService greetingService = context.getBean(GreetingService.class);
+        logger.info(greetingService.sayGreeting());  // "Greeting, user!"
+    }
+}
