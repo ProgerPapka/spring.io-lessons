a85ac1598ce465f9daf7ba113b5b060d42bad5ea
Эльдар <ziatdinov.eldar9@gmail.com>
2017-11-18 23:11:24 +0400

    Added for example @Required - field not null!!

--- src/main/java/lessons/two/services/GreetingServiceImpl.java
+++ src/main/java/lessons/two/services/GreetingServiceImpl.java
@@ -1,9 +1,18 @@
 package lessons.two.services;
 
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GreetingServiceImpl implements GreetingService {
+
+    private ApplicationContext context;
+
+    @Required
+    public void setContext(ApplicationContext context) {
+        this.context = context;
+    }
 
     @Override
     public String sayGreeting() {
