c25b2e589ea3ad798ea967e71d7dda37044eeaa0
Эльдар <ziatdinov.eldar9@gmail.com>
2017-11-18 23:27:09 +0400

    added scope and @Component

--- src/main/java/lessons/two/services/GreetingServiceImpl.java
+++ src/main/java/lessons/two/services/GreetingServiceImpl.java
@@ -1,5 +1,10 @@
 package lessons.two.services;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
 public class GreetingServiceImpl implements GreetingService {
 
     @Override
